1857e279c69f2ca0ffa43b02f6168b3bb8bfff63
raviswamiii <raviswamisamsung@gmail.com>
2025-05-16 16:43:20 +0530

    squareOfSortedARR

--- src/Array/TwoPointers/RemoveDuplicates.java
+++ src/Array/TwoPointers/RemoveDuplicates.java
@@ -1,35 +1,39 @@
 package Array.TwoPointers;
 
-public class RemoveDuplicates {
-    public static int removeDuplicates(int[] nums) {
-        // If array is empty or has only 1 element, it's already unique
-        if (nums.length == 0) return 0;
+// Brute Force ---
+// public class RemoveDuplicates {
+//    public static int removeDuplicates(int[] nums) {
+//        // If array is empty or has only 1 element, it's already unique
+//        if (nums.length == 0) return 0;
+//
+//        // Pointer i tracks the last unique element
+//        int i = 0;
+//
+//        // Start j from 1 and compare with i
+//        for (int j = 1; j < nums.length; j++) {
+//            // If a new unique element is found
+//            if (nums[j] != nums[i]) {
+//                i++; // move i forward
+//                nums[i] = nums[j]; // copy unique element to correct place
+//            }
+//        }
+//
+//        // i is index of last unique element, so total unique elements = i + 1
+//        return i + 1;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] nums = {1, 1, 2, 2, 3, 3, 4, 4, 5};
+//        int k = removeDuplicates(nums);
+//
+//        // Print first k elements
+//        for (int i = 0; i < k; i++) {
+//            System.out.print(nums[i] + " ");
+//        }
+//        System.out.println("\nUnique count: " + k);
+//    }
+//}
 
-        // Pointer i tracks the last unique element
-        int i = 0;
+// Optimized ---
 
-        // Start j from 1 and compare with i
-        for (int j = 1; j < nums.length; j++) {
-            // If a new unique element is found
-            if (nums[j] != nums[i]) {
-                i++; // move i forward
-                nums[i] = nums[j]; // copy unique element to correct place
-            }
-        }
-
-        // i is index of last unique element, so total unique elements = i + 1
-        return i + 1;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {1, 1, 2, 2, 3, 3, 4, 4, 5};
-        int k = removeDuplicates(nums);
-
-        // Print first k elements
-        for (int i = 0; i < k; i++) {
-            System.out.print(nums[i] + " ");
-        }
-        System.out.println("\nUnique count: " + k);
-    }
-}
 
--- src/Array/TwoPointers/SquareOfSortedArr.java
+++ src/Array/TwoPointers/SquareOfSortedArr.java
@@ -1,4 +1,38 @@
 package Array.TwoPointers;
 
 public class SquareOfSortedArr {
+    public static void main(String[] args) {
+        int[] nums = {-4, -1, 0, 3, 10};  // 🔹 This is where you give the input
+        SquareOfSortedArr obj = new SquareOfSortedArr();
+        int[] result = obj.sortedSquares(nums);
+
+        // Printing the output
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1;
+        int index = n - 1;
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[index] = leftSquare;
+                left++;
+            } else {
+                result[index] = rightSquare;
+                right--;
+            }
+            index--;
+        }
+
+        return result;
+    }
 }
+
