3bf4dbab77b17838b225743e9dd8225e309dfa04
raviswamiii <raviswamisamsung@gmail.com>
2025-06-16 13:41:14 +0530

    practiceSession

---
+++ src/Array/PracticeSession/TrappingRainWater.java
@@ -0,0 +1,35 @@
+package Array.PracticeSession;
+
+public class TrappingRainWater {
+    public int trap(int[] height){
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int totalWater = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= leftMax){
+                    leftMax = height[left];
+                }else{
+                    totalWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if(height[right] >= rightMax){
+                    rightMax = height[right];
+                } else{
+                    totalWater += rightMax - height[right];
+                }
+                right--;
+            }
+        } return totalWater;
+    }
+
+    public static void main(String[] args){
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        TrappingRainWater obj = new TrappingRainWater();
+        int result = obj.trap(height);
+        System.out.println("Total trapped water: " + result);
+    }
+}
