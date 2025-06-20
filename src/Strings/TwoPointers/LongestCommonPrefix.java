ad97dae16cbfaf31c13963a627b9e58bc40305b1
raviswamiii <raviswamisamsung@gmail.com>
2025-06-20 12:57:06 +0530

    bfLongestCommonPrefix

---
+++ src/Strings/TwoPointers/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+package Strings.TwoPointers;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs){
+        if(strs == null || strs.length == 0) return "";
+
+        for(int i = 0; i < strs[0].length(); i++){
+            char c = strs[0].charAt(i);
+
+            for(int j = 1; j < strs.length; j++){
+                if(i >= strs[j].length() || strs[j].charAt(i) != c){
+                    return strs[0].substring(0,i);
+                }
+            }
+        } return strs[0];
+    }
+
+    public static void main(String[] args){
+        String[] strs = {"flower","flow","flight"};
+        LongestCommonPrefix obj = new LongestCommonPrefix();
+        String part = obj.longestCommonPrefix(strs);
+        System.out.println(part);
+    }
+}
