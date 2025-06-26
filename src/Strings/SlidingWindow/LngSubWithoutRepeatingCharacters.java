4ce96de2ae24764d353a117668a5b973b8c8385e
raviswamiii <raviswamisamsung@gmail.com>
2025-06-26 12:26:39 +0530

    optLongestSubstringWithoutReapetingCharacters

--- src/Strings/SlidingWindow/LngSubWithoutRepeatingCharacters.java
+++ src/Strings/SlidingWindow/LngSubWithoutRepeatingCharacters.java
@@ -1,37 +1,66 @@
 package Strings.SlidingWindow;
 
+import java.util.HashSet;
+
 public class LngSubWithoutRepeatingCharacters {
-    public int longestSubstring(String s){
-       int maxLength = 0;
+//    public int longestSubstring(String s){
+//       int maxLength = 0;
+//
+//       for(int i = 0; i < s.length(); i++){
+//           for (int j = i; j < s.length(); j++) {
+//               if(allUnique(s,i,j)){
+//                   int length = j - i + 1;
+//                   if(length > maxLength){
+//                       maxLength = length;
+//                   }
+//               }
+//           }
+//       }
+//
+//       return maxLength;
+//    }
+//
+//    private boolean allUnique(String s, int start, int end){
+//        for (int i = start; i <= end; i++){
+//            for (int j = i + 1; j <= end; j++) {
+//                if(s.charAt(i) == s.charAt(j)){
+//                    return false;
+//                }
+//            }
+//        } return true;
+//    }
+//
+//    public static void main(String[] args){
+//        String s = "abcabcbb";
+//        LngSubWithoutRepeatingCharacters obj = new LngSubWithoutRepeatingCharacters();
+//        int lengthOfLongestSubstring = obj.longestSubstring(s);
+//        System.out.println(lengthOfLongestSubstring);
+//    }
 
-       for(int i = 0; i < s.length(); i++){
-           for (int j = i; j < s.length(); j++) {
-               if(allUnique(s,i,j)){
-                   int length = j - i + 1;
-                   if(length > maxLength){
-                       maxLength = length;
-                   }
-               }
-           }
-       }
 
-       return maxLength;
-    }
+    public int longestSubString(String s){
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, right = 0, maxLength = 0;
 
-    private boolean allUnique(String s, int start, int end){
-        for (int i = start; i <= end; i++){
-            for (int j = i + 1; j <= end; j++) {
-                if(s.charAt(i) == s.charAt(j)){
-                    return false;
-                }
+        while(right < s.length()){
+            char currCharacter = s.charAt(right);
+            while(set.contains(currCharacter)){
+                set.remove(s.charAt(left));
+                left++;
             }
-        } return true;
+
+            set.add(currCharacter);
+            maxLength = Math.max(maxLength, right - left + 1);
+            right++;
+        }
+
+        return maxLength;
     }
 
     public static void main(String[] args){
         String s = "abcabcbb";
         LngSubWithoutRepeatingCharacters obj = new LngSubWithoutRepeatingCharacters();
-        int lengthOfLongestSubstring = obj.longestSubstring(s);
-        System.out.println(lengthOfLongestSubstring);
+        int LongestUniqueSubstring = obj.longestSubString(s);
+        System.out.println(LongestUniqueSubstring);
     }
 }
